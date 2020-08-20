e5413cce59fd8f35c4b06d1fa3e1749f47bc3abb
SpyrosGeo <xarodasimba@gmail.com>
2020-08-20 18:12:04 +0300

    finished basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,22 @@ import RestaurantDetail from './routes/RestaurantDetail'
 import UpdatePage from './routes/UpdatePage'
 function App() {
   return (
-  	<Switch>
-	<Route exact path="/">
-		<Home />	
-	</Route>
-
-	<Route exact path="/restaurants/:id">
-		<RestaurantDetail />	
-	</Route>
-	<Route exact path="/restaurants/:id/update">
-		<UpdatePage />	
-	</Route>
-
-
-	  </Switch>
+  	<div class="container">
+		  <Switch>
+		  	<Route exact path="/">
+		  		<Home />	
+		  	</Route>
+		  
+		  	<Route exact path="/restaurants/:id">
+		  		<RestaurantDetail />	
+		  	</Route>
+		  	<Route exact path="/restaurants/:id/update">
+		  		<UpdatePage />	
+		  	</Route>
+		  
+		  
+		  	  </Switch>
+	  </div>
 
 
   )
